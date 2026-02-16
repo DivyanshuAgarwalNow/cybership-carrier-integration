81263ee6779d7c7f346e72b8734cbe3ae48a73b0
Divyanshu Agarwal <94756605+DivyanshuAgarwalNow@users.noreply.github.com>
2026-02-16 16:12:24 +0530

    ..

--- src/errors/carrier-api.error.ts
+++ src/errors/carrier-api.error.ts
@@ -1,1 +1,12 @@
+import { ErrorCode } from "./error.types";
 
+export class CarrierApiError extends Error {
+  constructor(
+    public code: ErrorCode,
+    message: string,
+    public status?: number,
+    public details?: unknown
+  ) {
+    super(message);
+  }
+}
